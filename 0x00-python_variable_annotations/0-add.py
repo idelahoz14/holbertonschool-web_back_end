e527712a125fb7d542170dc918683d8caedfe3cb
idelahoz14 <idelahoz14@cuc.edu.co>
2021-10-19 15:19:30 -0500

    FIX: pycode style

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Take two arguments and return their sum as a float """
 
+
 def add(a: float, b: float) -> float:
     """ Take two arguments and return their sum as a float """
     return a + b
