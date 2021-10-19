142b039cfc4206168c72bb32e89896e7d5378037
idelahoz14 <idelahoz14@cuc.edu.co>
2021-10-19 15:43:59 -0500

    ADD: basic annotation function with floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" This module contain a function that take a argument
+and returns the floor of the float """
+
+
+def floor(n: float) -> float:
+    """ Function that take a argument and returns the floor of the float """
+    return n // 1
