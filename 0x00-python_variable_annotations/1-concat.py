14e0e7cbe29e74f32c8eb82c7e0f29272644209a
idelahoz14 <idelahoz14@cuc.edu.co>
2021-10-19 15:30:56 -0500

    ADD: basic annotation function with strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Type-annotated function that takes two strings and returns a concatenates string """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ function that return concatenated string """
+    return "{}{}".format(str1, str2)
+    
