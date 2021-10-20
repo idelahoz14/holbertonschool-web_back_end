168ad112901f02ceac72efa56c892659753be323
idelahoz14 <idelahoz14@cuc.edu.co>
2021-10-19 21:27:45 -0500

    Fix

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,11 @@
-#!usr/bin/env python3
-""" Async function """
+#!/usr/bin/env python3
+""" Basic async function """
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """returns a delayed value"""
+    """ Basic async function """
     delayed_value = random.uniform(0, max_delay)
     await asyncio.sleep(delayed_value)
     return delayed_value
