f38c8e9e24238b31f15e5e2c09ab342cb0dedcf7
idelahoz14 <idelahoz14@cuc.edu.co>
2021-10-19 21:15:05 -0500

    Fix: pycode style

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Annotate the below function’s parameters and return values with the appropriate types  """
+""" Annotate the below function’s parameters
+and return values with the appropriate types  """
 from typing import Iterable, Sequence, List, Tuple
 
 
