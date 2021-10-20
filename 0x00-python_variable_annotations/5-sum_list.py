531a1a526f6dae871a3b6e2fc0788fdca1379b43
idelahoz14 <idelahoz14@cuc.edu.co>
2021-10-19 20:34:00 -0500

    ADD: basic annotation function with Lists

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" This module contain the function sum_list which takes a list input_list of
+floats as argument and returns their sum as a float. """
+from typing import List
+
+
+def sum_list(imput_list: List[float]) -> float:
+    """ function sum_list which takes a list input_list of
+    floats as argument and returns their sum as a float """
+    return sum(imput_list)
