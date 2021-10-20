279e17bec95ac17b62512db72786197aef3e6d55
idelahoz14 <idelahoz14@cuc.edu.co>
2021-10-19 20:43:01 -0500

    ADD: basic annotation function with Lists mixed

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""" This module contain the function sum_mixed_list which takes a
+list mxd_lst of integers and floats and returns their sum as a float """
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    return sum(mxd_lst)
