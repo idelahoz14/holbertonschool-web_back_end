60d2b87cc6b442952335c2559c02cd2f9e29ed6c
idelahoz14 <idelahoz14@cuc.edu.co>
2021-10-21 16:37:44 -0500

    Fix: pycodestyle

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-""" Given the parameters and the return values, add type annotations to the function """
+""" Given the parameters and the return values,
+add type annotations to the function """
 from typing import Mapping, Union, Any, T
 import types
 
 
-def safely_get_value(dct: Mapping, key: Union[Any], default: Union[T, type(None)]) -> Union[Any, T]:
-    """ Given the parameters and the return values, add type annotations to the function """
+def safely_get_value(dct: Mapping, key: Union[Any],
+                     default: Union[T, type(None)]) -> Union[Any, T]:
+    """ Given the parameters and the return values,
+    add type annotations to the function """
     if key in dct:
         return dct[key]
     else:
