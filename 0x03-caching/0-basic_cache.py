f5f171e4186391687f191115ae9d9c50a0625a13
idelahoz14 <idelahoz14@cuc.edu.co>
2021-10-25 15:14:18 -0500

    Fix: condiotions from methods

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -9,15 +9,13 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if key == None or item == None:
-            pass
-
-        self.cache_data.update({key: item})
+        if key and item:
+            self.cache_data.update({key: item})
 
     def get(self, key):
         """ Get an item by key
         """
-        if self.cache_data.get(key) is None:
-            return None
+        if key is not None:
+            return self.cache_data.get(key)
 
-        return self.cache_data.get(key)
+        return None
