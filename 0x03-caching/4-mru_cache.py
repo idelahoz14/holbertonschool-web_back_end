b4feab11654f8b1f338f054ca0d4981f67ee609e
idelahoz14 <idelahoz14@cuc.edu.co>
2021-10-25 21:44:21 -0500

    MRU caching

---
+++ 0x03-caching/4-mru_cache.py
@@ -0,0 +1,25 @@
+""" LRU caching """
+BaseCaching = __import__('base_caching').BaseCaching
+
+class MRUCache(BaseCaching):
+    """ LRU Caching """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            self.cache_data.update({key: item})
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                print(f"DISCARD: {self.last}")
+                del(self.cache_data[self.last])
+            self.last = key
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is not None:
+            return self.cache_data.get(key)
+
+        return None
