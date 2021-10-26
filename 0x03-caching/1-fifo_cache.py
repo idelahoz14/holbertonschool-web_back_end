1d457187166e743a223dad0ef3bbce64e1d23cfd
idelahoz14 <idelahoz14@cuc.edu.co>
2021-10-25 21:02:26 -0500

    Try annotation function

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -4,7 +4,7 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 class FIFOCache(BaseCaching):
     """ FIFOcache """
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
 
     def put(self, key, item):
