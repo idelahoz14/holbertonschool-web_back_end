102f9cd7287e040f16e7331a98b37c42cbce751d
idelahoz14 <idelahoz14@cuc.edu.co>
2021-11-03 15:09:13 -0500

    Add: Auth class

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+
+class Auth:
+    """
+    """
+
+
+    @property
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        """
+        return False
+
+
+    @property
+    def authorization_header(self, request=None) -> str:
+        """
+        """
+        return None
+
+
+    @property
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        """
+        return None
