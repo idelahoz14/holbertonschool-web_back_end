6526ecd0c91750d5212b97b23ec1aa0719c9a0b5
idelahoz14 <idelahoz14@cuc.edu.co>
2021-11-23 22:28:23 -0500

    Fix

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -14,6 +14,7 @@ class TestGithubOrgClient(unittest.TestCase):
         ("google"),
         ("abc"),
     ])
+    
     @patch("client.get_json", return_value={"payload": True})
     def test_org(self, org_name, mock_get):
         """test_org"""
@@ -52,6 +53,7 @@ class TestGithubOrgClient(unittest.TestCase):
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False),
     ])
+
     def test_has_license(self, repo, license_key, expected_return):
         """ test GithubOrgClient.has_license"""
         test_client = GithubOrgClient("twitter")
@@ -59,30 +61,31 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(expected_return, test_return)
 
 
-@parameterized_class(
-    ('org_payload', 'repos_payload', 'expected_repos', 'apache2_repos'),
-    TEST_PAYLOAD
-)
-class TestIntegrationGithubOrgClient(unittest.TestCase):
-    """ class TestIntegrationGithubOrgClient """
-    @classmethod
-    def setUpClass(cls):
-        """set up class"""
-        cls.get_patcher = patch('requests.get', side_effect=HTTPError)
+    @parameterized_class(
+        ('org_payload', 'repos_payload', 'expected_repos', 'apache2_repos'),
+        TEST_PAYLOAD
+    )
+    
+    class TestIntegrationGithubOrgClient(unittest.TestCase):
+        """ class TestIntegrationGithubOrgClient """
+        @classmethod
+        def setUpClass(cls):
+            """set up class"""
+            cls.get_patcher = patch('requests.get', side_effect=HTTPError)
 
-    @classmethod
-    def tearDownClass(cls):
-        """tear down class"""
-        cls.get_patcher.stop()
+        @classmethod
+        def tearDownClass(cls):
+            """tear down class"""
+            cls.get_patcher.stop()
 
-    def test_public_repos_with_license(self):
-        """ public repos test """
-        test_client = GithubOrgClient("repo")
-        self.assertEqual(test_client.org, self.org_payload)
-        self.assertEqual(test_client.repos_payload, self.repos_payload)
-        self.assertEqual(test_client.public_repos(), self.expected_repos)
-        self.assertEqual(test_client.public_repos("NONEXISTENT"), [])
-        self.assertEqual(test_client.public_repos(
-            "apache-2.0"), self.apache2_repos)
-        self.get.assert_has_calls([call("https://api.github.com/orgs/repo"),
-                                   call(self.org_payload["repos_url"])])
+        def test_public_repos_with_license(self):
+            """ public repos test """
+            test_client = GithubOrgClient("repo")
+            self.assertEqual(test_client.org, self.org_payload)
+            self.assertEqual(test_client.repos_payload, self.repos_payload)
+            self.assertEqual(test_client.public_repos(), self.expected_repos)
+            self.assertEqual(test_client.public_repos("NONEXISTENT"), [])
+            self.assertEqual(test_client.public_repos(
+                "apache-2.0"), self.apache2_repos)
+            self.get.assert_has_calls([call("https://api.github.com/orgs/repo"),
+                                    call(self.org_payload["repos_url"])])
